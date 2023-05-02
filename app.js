64758f6a8952c8da97ed72ace3b4f503d348fe9b
JordanLDiaz <jordanldiaz29@gmail.com>
2023-05-02 10:00:21 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const boss = {
+  name: "Squirrel Mafia",
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1,
+  image: "assets/squirrel-mafia.gif"
+}
+
+const hero = {
+  name: 'SuperCat',
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1,
+  image: "assets/superCat.gif"
+}
+
+// function drawBoss() {
+//   let bossTemplate = ''
+//   bossTemplate +=`
+  
+//   `
+// }
